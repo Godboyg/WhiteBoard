bad84e03c11459f47c7dd0ea3aa2a6ac195d95fe
kushal <kushalsinghnegi72@gmail.com>
2025-09-22 11:33:52 +0530

    cors configured

--- server/dist/server.js
+++ server/dist/server.js
@@ -4,11 +4,13 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 const app = express();
 const PORT = 4000;
-app.use(cors());
+app.use(cors({
+    origin: "https://white-board-client-4cqa546mb-godboygs-projects.vercel.app"
+}));
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://white-board-client-4cqa546mb-godboygs-projects.vercel.app",
         methods: ["POST", "GET"]
     }
 });
